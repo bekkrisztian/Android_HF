abab9eb4231600a9bca31f90e390b87d85182d9d
bekkrisztian <benedekkrisztian@github.ro>
2020-10-17 11:17:06 +0300

    hazi3

---
+++ HF3/app/src/main/java/com/example/hf3/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.hf3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    Intiger[] imageArray = {
+R.drawable.
+    }
+
+    ListView
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
